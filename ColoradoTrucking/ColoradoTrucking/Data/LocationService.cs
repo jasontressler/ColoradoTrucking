55d112b33574a72d9fe62e81c326c81eaf3565a2
jasontressler <jasontressler@gmail.com>
2020-02-10 23:22:47 -0500

    Better I think?

---
+++ ColoradoTrucking/ColoradoTrucking/Data/LocationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ColoradoTrucking.Data {
+    public class LocationService : ILocationService
+    {
+        private readonly HttpClient _httpClient;
+
+        public LocationService(HttpClient httpClient) {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("http://locationclient.azurewebsites.net/");
+        }
+
+        public async Task<List<string>> GetCompanyNames(string search) {
+            using var response = await _httpClient.GetAsync($"search/?query={search}");
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<List<String>>(responseStream);
+        }
+    }
+}
