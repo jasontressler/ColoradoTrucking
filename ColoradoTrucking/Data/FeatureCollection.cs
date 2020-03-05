24765cc6dcd8cc99332344497ccf9281c4c5bce5
jasontressler <jasontressler@gmail.com>
2020-03-05 09:04:58 -0500

    resync

---
+++ ColoradoTrucking/Data/FeatureCollection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace ColoradoTrucking.Data {
+
+    public class FeatureCollection {
+        public string type = "Feature Collection";
+        public List<Feature> Features { get; set; }
+
+        public FeatureCollection(List<Feature> feat) {
+            Features = feat;
+        }
+    }
+
+    [ComplexType]
+    public class Feature {
+        private string type = "Feature";
+        public Geometry Geometry { get; set; }
+        public Properties Properties { get; set; }
+        public string Type { get => type; set => type = value; }
+
+        public Feature(Geometry g, Properties p) {
+            Geometry = g;
+            Properties = p;
+        }
+
+    }
+
+    [ComplexType]
+    public class Properties {
+        [Column("LEGAL_NAME")]
+        public string Name { get; set; }
+        [Column("PHY_STREET")]
+        public string Street { get; set; }
+        [Column("PHY_CITY")]
+        public string City { get; set; }
+        [Column("PHY_STATE")]
+        public string State { get; set; }
+        [Column("PHY_ZIP")]
+        public string Zip { get; set; }
+        [Column("TELEPHONE")]
+        public string Phone { get; set; }
+        [Column("EMAIL_ADDRESS")]
+        public string Email { get; set; }
+
+        public Properties(string name, string street, string city, string state, string zip, string phone, string email) {
+            this.Name = name;
+            this.Street = street;
+            this.City = city;
+            this.State = state;
+            this.Zip = zip;
+            this.Phone = phone;
+            this.Email = email;
+        }
+    }
+
+    [ComplexType]
+    public class Geometry {
+        private string type = "Point";
+
+        [Column("GPSLatitude"), JsonIgnore]
+        public double Lat { get; set; }
+
+        [Column("GPSLongitude"), JsonIgnore]
+        public double Lng { get; set; }
+
+        public double[] Coordinates { get; set; }
+        public string Type { get => type; set => type = value; }
+
+        public Geometry() {
+            Coordinates = new double[] { Lat, Lng };
+        }
+    }
+    
+}
---
+++ ColoradoTrucking/wwwroot/js/GoogleMap.js
@@ -0,0 +1,93 @@
+﻿function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 39, lng: -105.7821 },
+        zoom: 6.9, minZoom: 6.9, mapTypeControl: false, streetViewControl: false,
+    });
+    
+    var script = document.createElement('script');
+    script.src ='../js/Markers.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+
+    var bounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(36.998440, -109.045658),
+        new google.maps.LatLng(41.002379, -102.051562)
+    )
+
+    var CoBounds = [
+        { lat: 36.998440, lng: -109.045658 },
+        { lat: 40.999593, lng: -109.049950 },
+        { lat: 41.002379, lng: -102.051562 },
+        { lat: 36.992990, lng: -102.041874 }
+    ]
+    var WorldBounds = [ // covers the (mercator projection) world
+        new google.maps.LatLng(85, 180),
+        new google.maps.LatLng(85, 90),
+        new google.maps.LatLng(85, 0),
+        new google.maps.LatLng(85, -90),
+        new google.maps.LatLng(85, -180),
+        new google.maps.LatLng(0, -180),
+        new google.maps.LatLng(-85, -180),
+        new google.maps.LatLng(-85, -90),
+        new google.maps.LatLng(-85, 0),
+        new google.maps.LatLng(-85, 90),
+        new google.maps.LatLng(-85, 180),
+        new google.maps.LatLng(0, 180),
+        new google.maps.LatLng(85, 180)
+    ];
+
+    var boundPoly = new google.maps.Polygon({
+        paths: [WorldBounds, CoBounds],
+        strokeColor: '#FF0000',
+        strokeOpacity: .8,
+        strokeWeight: 1.5,
+        fillOpacity: .3
+    });
+
+    boundPoly.setMap(map);
+
+    map.addListener('dragend', function () {
+        if (bounds.contains(map.getCenter())) return;
+
+        var c = map.getCenter(),
+            x = c.lng(),
+            y = c.lat(),
+            maxX = bounds.getNorthEast().lng(),
+            maxY = bounds.getNorthEast().lat(),
+            minX = bounds.getSouthWest().lng(),
+            minY = bounds.getSouthWest().lat();
+
+        if (x < minX) x = minX;
+        if (x > maxX) x = maxX;
+        if (y < minY) y = minY;
+        if (y > maxY) y = maxY;
+
+        map.setCenter(new google.maps.LatLng(y, x));
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    window.eqfeed_callback = function (results) {
+        for (var i = 0; i < results.features.length; i++) {
+            var coords = results.features[i].geometry.coordinates;
+            var latLng = new google.maps.LatLng(coords[0], coords[1]);
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map
+            });
+            marker.addListener('mouseover', function () {
+                infowindow.setContent(
+                    '<h3>' + 'Hello' + '</h3>' +
+                    '<h4>' + f.getProperty('street') + '</h4>' +
+                    '<h4>' + f.getProperty('city') + ', ' + f.getProperty('state') + ' ' + f.getProperty('zip') + '</h4>' +
+                    '<h4>' + f.getProperty('phone') + '</h4>' +
+                    '<h4>' + f.getProperty('email') + '</h4>'
+                );
+                infowindow.open(map, marker);
+            });
+            marker.addListener('mouseout', function () {
+                infowindow.close();
+            });
+                
+        }
+    }
+}
